16fa6db173e0d0eaf4d21bac31fe9798a3daf423
channasorn chungmarisakul <63010185@KMITL.AC.TH>
2020-09-13 19:33:39 +0700

    Add project files.

---
+++ WORK5.1.1/Source.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void Aa();
+char ss,SS;
+char* p, str[100];
+int main() {
+	p = str;
+	scanf("%s", str, 99);//ป้อนได้ไม่เกิน99ตัว
+	Aa();
+}
+void Aa() {
+	while (*p != '\0') {
+		if (*p >= 65 && *p <= 90) {
+			ss = *p - 'A' + 'a';
+			printf("%c", ss);
+		}
+		if (*p >= 97 && *p <= 122) {
+			SS = *p - 'a' + 'A';
+			printf("%c", SS);
+		}
+		p++;
+	}
+}
